1118b82d595be69b9af82ffdfdefbb17069ff136
lucas <nesgnimmehl@gmail.com>
2019-04-13 13:25:54 -0300

    Python server endpoints

--- py/app.py
+++ py/app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
+from time import sleep
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/ping', methods = ['GET'])
 def ping():
-	return "Ping python\n"
+	return jsonify(ping=1), 200
+
+
+@app.route('/proxy/<float:timeout>', methods = ['GET'])
+def timeout(timeout):
+	sleep(timeout)
+	return jsonify(timeout=timeout), 200
+
+
+@app.route('/fibonacci/<int:n>', methods = ['GET'])
+def fibonacci(n):	
+	return jsonify(result=fibonacci_recursivo(n)), 200
+
+def fibonacci_recursivo(n):
+	if n <= 1:
+		return n
+	return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
