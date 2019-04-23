420b2979aa0abe4c04d50603987054f8ab7bc617
dcassinotti <damiancassinotti.33@gmail.com>
2019-04-22 23:33:47 -0300

    Agrego endpoint modificados + link al informe

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Punto de partida para el TP 1 de Arquitectura de Software (75.73) del 1er cuatrimestre de 2019
 
+# Link al Informe: https://docs.google.com/document/d/158WffnjLjz78TAHeITrN7TANtR_pXM4sxCTCQNWAL5k/edit?usp=sharing
+
 > **La fecha de entrega para el informe y el código es el jueves 25/04** :bangbang:
 
 La forma de entrega será crear un canal **privado** en Slack (llamado como el grupo) con todos los miembros del grupo y todos los docentes, y poner ahí un link al repositorio con el código (en caso de ser privado, invitar también a todos los docentes) y el informe (o avisar si está en el repositorio).
--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const MAX = 10000000000;
 
-app.get("/", (req, resp) => resp.status(200).send("Ping!"));
+app.get("/ping", (req, res) => res.status(200).json({'ping': 1}));
 
-app.get("/timeout", (req, res) => setTimeout(() => res.status(200).send('End timeout'), 5000));
-
-app.get("/load", (req, res) => {
-	for (var i = 0; i < MAX; i++) {}
-	res.status(200).send(`Load! ${MAX}`);
+app.get("/timeout/:timeout", (req, res) => {
+	var time = req.params.timeout;
+	setTimeout(() => res.status(200).json({'timeout': time}), time*1000);
 });
+
+app.get("/fibonacci/:n", (req, res) => {
+	var n = req.params.n;
+	res.status(200).json({'result': fibonacci(parseInt(n))});
+});
+
+var fibonacci = (n) => {
+	if (n <= 1)
+		return n;
+	return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
 app.listen(port,() => console.log(`Escuchando en el puerto ${port}`));
