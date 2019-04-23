f65ef79da153210a60314796756035fb1ca265ef
dcassinotti <damiancassinotti.33@gmail.com>
2019-04-23 01:49:27 -0300

    Agrego el prefijo node y gunicorn a los endpoints

--- py/app.py
+++ py/app.py
@@ -3,19 +3,19 @@ from time import sleep
 
 app = Flask(__name__)
 
-@app.route('/ping', methods = ['GET'])
+@app.route('/gunicorn/ping', methods = ['GET'])
 def ping():
 	return jsonify(ping=1), 200
 
 
-@app.route('/proxy/<float:timeout>', methods = ['GET'])
+@app.route('/gunicorn/proxy/<float:timeout>', methods = ['GET'])
 def timeout(timeout):
 	sleep(timeout)
 	return jsonify(timeout=timeout), 200
 
 
-@app.route('/fibonacci/<int:n>', methods = ['GET'])
-def fibonacci(n):	
+@app.route('/gunicorn/fibonacci/<int:n>', methods = ['GET'])
+def fibonacci(n):
 	return jsonify(result=fibonacci_recursivo(n)), 200
 
 def fibonacci_recursivo(n):
